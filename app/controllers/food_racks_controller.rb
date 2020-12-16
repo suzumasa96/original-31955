2c6bc75cf4a6d839470817d9c37f57864d3510ad
suzumasa96 <73861844+suzumasa96@users.noreply.github.com>
2020-12-16 23:24:45 +0900

    実装１

--- app/controllers/food_racks_controller.rb
+++ app/controllers/food_racks_controller.rb
@@ -1,8 +1,24 @@
 class FoodRacksController < ApplicationController
   def index
+    @food_racks = FoodRack.all
   end
-  
-  def create
 
+  def new
+    @food_rack = FoodRack.new
+  end
+
+  def create
+    @food_rack = FoodRack.new(foodrack_params)
+    if @food_rack.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def foodrack_params
+    params.require(:food_rack).permit(:food_name,:picture,:category_id,:food_deadline,:memo).merge(user_id: current_user.id)
   end
 end
