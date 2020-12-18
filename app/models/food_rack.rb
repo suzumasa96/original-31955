41c49fc9937f49d1fad77c7e52fa18dcc32a6080
suzumasa96 <73861844+suzumasa96@users.noreply.github.com>
2020-12-18 20:13:35 +0900

    外見装飾１

--- app/models/food_rack.rb
+++ app/models/food_rack.rb
@@ -5,6 +5,5 @@ class FoodRack < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
-  validates :memo, presence: true
   validates :category_id, numericality: { other_than: 1 }
 end
