f7d2abd97dd3d42a9fc4298b498a7f17f90594f0
suzumasa96 <73861844+suzumasa96@users.noreply.github.com>
2020-12-22 17:57:36 +0900

    外見装飾３

--- app/models/food_rack.rb
+++ app/models/food_rack.rb
@@ -2,6 +2,14 @@ class FoodRack < ApplicationRecord
   belongs_to :user
   has_one_attached :picture
 
+  def self.search(search)
+    if search != ""
+      FoodRack.where('text LIKE(?)',"%#{search}%")
+    else
+      FoodRack.all
+    end
+  end
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'food_racks#index'
-  resources :food_racks
+  resources :food_racks do
+    collection do
+      get 'search'
+    end
+  end
 end
