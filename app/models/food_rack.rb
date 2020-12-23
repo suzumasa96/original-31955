7ca26ad3c5e616fe549784f4944cc73b1c87fdb5
suzumasa96 <73861844+suzumasa96@users.noreply.github.com>
2020-12-23 22:42:18 +0900

    写真必須機能実装

--- app/models/food_rack.rb
+++ app/models/food_rack.rb
@@ -2,18 +2,23 @@ class FoodRack < ApplicationRecord
   belongs_to :user
   has_one_attached :picture
 
+  def was_attached?
+    self.image.attached?
+  end
+
   def previous
     FoodRack.where("id < ?", self.id).order("id DESC").first
   end
   def next
     FoodRack.where("id > ?", self.id).order("id ASC").first
   end
-  
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :month
   belongs_to :day
 
+  validates :picture, presence: true
   validates :category_id, numericality: { other_than: 1 }
   validates :month_id, numericality: { other_than: 1 }
   validates :day_id, numericality: { other_than: 1 }
