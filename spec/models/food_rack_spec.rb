bc4cdbece8d8d8a10285d9263f82f1c1a0fcc7f1
suzumasa96 <73861844+suzumasa96@users.noreply.github.com>
2021-02-03 11:49:43 +0900

    実装

--- spec/models/food_rack_spec.rb
+++ spec/models/food_rack_spec.rb
@@ -6,36 +6,36 @@ RSpec.describe FoodRack, type: :model do
   end
 
   describe '食材情報の保存' do
-    context "食材情報の保存ができるとき" do
-      it "食材の写真と保存場所、保存月日があれば保存ができる" do
-        expect(@food_rack).to be_valid
-      end
-    end
-
     context "食材情報の保存ができない時" do
       it "食材の写真がないと保存できない" do
         @food_rack.picture = nil
-        expect(@food_rack.errors.full_messages).to include
+        @food_rack.valid?
+        expect(@food_rack.errors.full_messages).to include("食材の写真を入力してください")
       end
       it "保存場所情報がないと登録できない" do
         @food_rack.category_id = nil
         @food_rack.valid?
-        expect(@food_rack.errors.full_messages).to include("Memo ")
+        expect(@food_rack.errors.full_messages).to include(" ")
+      end
+      it "保存年情報がないと登録できない" do
+        @food_rack.year_id = nil
+        @food_rack.valid?
+        expect(@food_rack.errors.full_messages).to include(" ")
       end
       it "保存月情報がないと登録できない" do
         @food_rack.month_id = nil
         @food_rack.valid?
-        expect(@food_rack.errors.full_messages).to include("Memo ")
+        expect(@food_rack.errors.full_messages).to include(" ")
       end
       it "保存日情報がないと登録できない" do
         @food_rack.day_id = nil
         @food_rack.valid?
-        expect(@food_rack.errors.full_messages).to include("Memo ")
+        expect(@food_rack.errors.full_messages).to include(" ")
       end
       it "ユーザーが紐づいていないと登録できない" do
         @food_rack.user = nil
         @food_rack.valid?
-        expect(@food_rack.errors.full_messages).to include("User must exist")
+        expect(@food_rack.errors.full_messages).to include(" ")
       end
     end
   end
